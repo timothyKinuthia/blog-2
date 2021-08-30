06bc747d0056e34a4806be7b2887c8f186a152dd
timothy <timothy9369@gmail.com>
2021-08-30 21:50:07 +0300

    email verification

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -4,7 +4,9 @@ import bcrypt from "bcrypt";
 //modules
 import User from "../models/userModel";
 import { generateActiveToken } from "./generateToken";
+import sendMail from "../config/sendMail";
 
+const CLIENT_URL = `${process.env.BASE_URL}`;
 
 const authCtr = {
   registerUser: async (req: Request, res: Response, next: NextFunction) => {
@@ -25,6 +27,14 @@ const authCtr = {
       
       //generate active token
       const activeToken = await generateActiveToken(newUser._id);
+
+      // //send mail
+      // const url = `${CLIENT_URL}/active/${activeToken}`;
+
+      // if (account) {
+      //   await sendMail(account, url, "Verify your email");
+      //   return res.json({ msg: "Success! Please check your email to complete signup." });
+      // }
       
       //new user and active token
       res.status(200).json({ msg: "registered successfully", user: newUser, activeToken });
